5c7c1a6bf758a58a744a620f2d1320378970631e
RayAra2004 <raynan.araujo@gmail.com>
2023-04-07 19:57:22 -0300

    Imagem dos Parrots

--- script/script.js
+++ script/script.js
@@ -1,21 +1,35 @@
 let qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
 const divCartas = document.querySelector('.cartas');
-const parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot']
+const parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
+parrots.sort(comparador);
+
 
 while(qtdCartas < 4 || qtdCartas > 14 || qtdCartas%2 !== 0){
     qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
 }
 
-for(i=0; qtdCartas/2 > i; i++){
-    for(j=0; j < 2; j++){
-        divCartas.innerHTML +=  `
-            <div class="card">
-                <div class="front-face face">
-                <img src="./imgs/back.png" alt="">
-                </div>
-                <div class="back-face face">
-                ${i}
-                </div>
-            </div>`;
-    }
+let imgsSelecionadas = parrots.slice(0, qtdCartas/2);
+
+for (let index = 0; index < qtdCartas/2; index++) {
+    imgsSelecionadas.push(imgsSelecionadas[index]); 
+}
+
+imgsSelecionadas.sort(comparador);
+
+for(j=0; j < imgsSelecionadas.length; j++){
+    divCartas.innerHTML +=  `
+        <div class="card">
+            <div class="front-face face">
+            <img src="./imgs/back.png" alt="">
+            </div>
+            <div class="back-face face">
+            <img src="./imgs/${imgsSelecionadas[j]}.gif" alt="">
+            </div>
+        </div>`;
+}
+
+
+
+function comparador(){
+    return Math.random() - 0.5; 
 }
