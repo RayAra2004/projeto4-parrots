cd18207ba70cb9f84661fcfbea355e44c83fdc49
RayAra2004 <raynan.araujo@gmail.com>
2023-04-07 15:50:57 -0300

    Distribuição das cartas

--- script/script.js
+++ script/script.js
@@ -1,22 +1,21 @@
 let qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
-let cont = 0;
-
 const divCartas = document.querySelector('.cartas');
+const parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot']
 
-while(qtdCartas < 3 && qtdCartas > 14 && qtdCartas%2 !== 0){
+while(qtdCartas < 4 || qtdCartas > 14 || qtdCartas%2 !== 0){
     qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
 }
 
-if(qtdCartas > 3 && qtdCartas <= 14 && qtdCartas%2 === 0){
-    for(i=0; qtdCartas > i; i++){
+for(i=0; qtdCartas/2 > i; i++){
+    for(j=0; j < 2; j++){
         divCartas.innerHTML +=  `
-        <div class="card">
-            <div class="front-face face">
-            <img src="./imgs/back.png" alt="">
-            </div>
-            <div class="back-face face">
-            Verso
-            </div>
-        </div>`;
+            <div class="card">
+                <div class="front-face face">
+                <img src="./imgs/back.png" alt="">
+                </div>
+                <div class="back-face face">
+                ${i}
+                </div>
+            </div>`;
     }
 }
