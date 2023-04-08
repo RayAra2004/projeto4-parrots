6e7881bfa149a2a656dd29e3ba2bbbd1b7b83cbf
RayAra2004 <raynan.araujo@gmail.com>
2023-04-08 00:14:28 -0300

    Correção de bug (msm carta contando 2 vzs)

--- script/script.js
+++ script/script.js
@@ -1,38 +1,41 @@
 let qtdCartasViradas = 0, qtdJogadas = 0, qtdPares = 0, tempo = 0, qtdCartas =0, carta1, carta2;
 const divRelogio = document.querySelector('.relogio');
+const divCartas = document.querySelector('.cartas');
 let idTime;
 
-qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
-const divCartas = document.querySelector('.cartas');
-const parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
-parrots.sort(comparador);
+jogo();
 
-while(qtdCartas < 4 || qtdCartas > 14 || qtdCartas%2 !== 0){
+function jogo(){
     qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
+    const parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
+    parrots.sort(comparador);
+
+    while(qtdCartas < 4 || qtdCartas > 14 || qtdCartas%2 !== 0){
+        qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
+    }
+
+    let imgsSelecionadas = parrots.slice(0, qtdCartas/2);
+
+    for (let index = 0; index < qtdCartas/2; index++) {
+        imgsSelecionadas.push(imgsSelecionadas[index]); 
+    }
+
+    imgsSelecionadas.sort(comparador);
+
+    for(j=0; j < imgsSelecionadas.length; j++){
+        divCartas.innerHTML +=  `
+            <div onclick="virar(this)" class="card">
+                <div class="front-face face">
+                <img src="./imgs/back.png" alt="">
+                </div>
+                <div class="back-face face">
+                <img src="./imgs/${imgsSelecionadas[j]}.gif" alt="">
+                </div>
+            </div>`;
+    }
+
+    idTime = setInterval(updateRelogio, 1000);
 }
-
-let imgsSelecionadas = parrots.slice(0, qtdCartas/2);
-
-for (let index = 0; index < qtdCartas/2; index++) {
-    imgsSelecionadas.push(imgsSelecionadas[index]); 
-}
-
-imgsSelecionadas.sort(comparador);
-
-for(j=0; j < imgsSelecionadas.length; j++){
-    divCartas.innerHTML +=  `
-        <div onclick="virar(this)" class="card">
-            <div class="front-face face">
-            <img src="./imgs/back.png" alt="">
-            </div>
-            <div class="back-face face">
-            <img src="./imgs/${imgsSelecionadas[j]}.gif" alt="">
-            </div>
-        </div>`;
-}
-
-idTime = setInterval(updateRelogio, 1000);
-
 
 function comparador(){
     return Math.random() - 0.5; 
@@ -53,15 +56,16 @@ function virar(card){
         carta2 = card;
 
         comparaCartas();
-   
+
     }
 }
 
 function comparaCartas(){
-   if(carta1.children[1].children[0].attributes[0].value === carta2.children[1].children[0].attributes[0].value){
+   if(carta1.children[1].children[0].attributes[0].value === carta2.children[1].children[0].attributes[0].value && carta1 !== carta2){
         qtdCartasViradas = 0;
         carta1.removeAttribute('onclick');
         carta2.removeAttribute('onclick');
+
         qtdPares++;
         if(qtdCartas/2 === qtdPares){
             clearInterval(idTime);
@@ -89,12 +93,17 @@ function updateRelogio(){
 }
 
 function reiniciaJogo(){
-    let resposta = prompt("Deseja jogar novamente?");
+    let resposta = prompt("Você gostaria de reiniciar a partida? (sim ou não)");
     while(resposta !== "sim"){
-        resposta = prompt("Deseja jogar novamente?");
+        if(resposta === 'não'){
+            break;
+        }
+        resposta = prompt("Você gostaria de reiniciar a partida? (sim ou não)");
     }
 
     if(resposta === "sim"){
+        divCartas.innerHTML = '';
+        qtdCartasViradas = 0, qtdJogadas = 0, qtdPares = 0, tempo = 0, qtdCartas = 0;
         jogo();
     }
 }
