c13559fd1920b3ce37948579834f0671c07089dc
RayAra2004 <raynan.araujo@gmail.com>
2023-04-07 23:01:55 -0300

    Fim do Jogo

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
+let qtdCartasViradas = 0, qtdJogadas = 0, qtdPares = 0, carta1, carta2;
 let qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
 const divCartas = document.querySelector('.cartas');
 const parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
 parrots.sort(comparador);
-
 
 while(qtdCartas < 4 || qtdCartas > 14 || qtdCartas%2 !== 0){
     qtdCartas = Number(prompt('Digite a quantidade de cartas.'));
@@ -18,7 +18,7 @@ imgsSelecionadas.sort(comparador);
 
 for(j=0; j < imgsSelecionadas.length; j++){
     divCartas.innerHTML +=  `
-        <div class="card">
+        <div onclick="virar(this)" class="card">
             <div class="front-face face">
             <img src="./imgs/back.png" alt="">
             </div>
@@ -28,8 +28,49 @@ for(j=0; j < imgsSelecionadas.length; j++){
         </div>`;
 }
 
-
-
 function comparador(){
     return Math.random() - 0.5; 
+}
+
+function virar(card){
+    qtdCartasViradas++;
+    if(qtdCartasViradas === 1){
+        card.children[0].classList.add('virada');
+        card.children[1].classList.add('virada2');
+        qtdJogadas++;
+        carta1 = card;
+
+    }else if(qtdCartasViradas ===2){
+        card.children[0].classList.add('virada');
+        card.children[1].classList.add('virada2');
+        qtdJogadas++;
+        carta2 = card;
+
+        comparaCartas();
+   
+    }
+}
+
+function comparaCartas(){
+   if(carta1.children[1].children[0].attributes[0].value === carta2.children[1].children[0].attributes[0].value){
+        qtdCartasViradas = 0;
+        carta1.removeAttribute('onclick');
+        carta2.removeAttribute('onclick');
+        qtdPares++;
+        if(qtdCartas/2 === qtdPares){
+            setTimeout(function(){
+                alert(`Você ganhou em ${qtdJogadas} jogadas!`)
+            }, 500);
+        }
+   }else{
+        setTimeout(desvirar, 1000);
+   }
+}
+
+function desvirar(){
+    carta1.children[0].classList.toggle('virada');
+    carta1.children[1].classList.toggle('virada2');
+    carta2.children[0].classList.toggle('virada');
+    carta2.children[1].classList.toggle('virada2');
+    qtdCartasViradas = 0;
 }
